989ffc72f1b9e6298655f051fb6040ec3d7133d1
Osvaldo Luna <osvaldounam18@gmail.com>
2017-11-16 19:37:23 -0600

    Se piden la medida de los catetos, y posteriormente se calcula la hipotenusa

---
+++ Pitagoras/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pitagoras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Para el teorema de pitagoras se necesita:");
+            Console.WriteLine("Medida del cateto opuesto:");
+            int co = int.Parse(Console.ReadLine());
+            Console.WriteLine("Medida del cateto adyacente:");
+            int ca = int.Parse(Console.ReadLine());
+            Console.WriteLine(" |\" ");
+            Console.WriteLine(" | \" ");
+            Console.WriteLine(" |  \" ");
+            Console.WriteLine(" |___\" ");
+            Console.WriteLine("El teorema es: {0}"+ co*ca);
+            Console.ReadKey();
+        }
+    }
+}
